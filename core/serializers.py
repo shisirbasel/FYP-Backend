7f034123ef2d9c7f428a95937dc1089294891fbd
Shisir Basel <110526802+shisirbasel@users.noreply.github.com>
2024-03-10 08:16:12 +0545

    Bug fixes, Like Book Backend and Get Genres Backend

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from core.models import Book
+from core.models import Book, Genre, User
+
+
+class GetGenresSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Genre
+         fields = ['id','name']
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -11,6 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
             password=validated_data['password'],
             first_name=validated_data.get('first_name', ""),
             last_name=validated_data.get('last_name', ""),
+            username=validated_data.get('username', ""),
         )
 
         user.save()
@@ -18,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ['email', 'password', 'first_name', 'last_name',]  
+        fields = ['email', 'password', 'first_name', 'last_name','username']  
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -41,19 +48,25 @@ class ProfilePictureSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-    
-    username = serializers.CharField(source='user.username', read_only=True)
+    class Meta:
+
+        model = Book
+        fields = ['title', 'author', 'is_traded', 'genre', 'upload_date', 'user', 'image']
+        read_only_fields = ['user']
+
+class ShowBookSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Book
-        fields = ['title', 'author', 'is_traded', 'genre', 'upload_date', 'user', 'username', 'image']
-        read_only_fields = ['user', 'username']
+        fields = ['id','title', 'author', 'is_traded', 'genre', 'upload_date', 'user', 'image']
 
-
-    
 class ProfileSerializer(serializers.ModelSerializer):
+    
     class Meta:
-        model =get_user_model()
-        fields = "__all__"
+        model = get_user_model()
+        fields = ['first_name','last_name','username','email','profile_picture','genre']
+        read_only_fields = ['profile_picture']
+
 
 class UpdatePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
@@ -62,3 +75,15 @@ class UpdatePasswordSerializer(serializers.Serializer):
 class VerifyAccountSerializer(serializers.Serializer):
     email = serializers.EmailField()
     otp = serializers.CharField()
+
+class LikeBookSerializer(serializers.Serializer):
+    book_id = serializers.IntegerField()
+
+    def validate_book_id(self, value):
+        try:
+            book = Book.objects.get(pk=value)
+        except Book.DoesNotExist:
+            raise serializers.ValidationError("Invalid book ID")
+
+        return value
+
--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,31 @@
 from django.urls import path
-from core.views import RegisterUserView,ShowUsersView,UploadProfilePictureView, LoginUserView, AddBookView, \
-            UpdateBookView,ShowBooksView,DeleteBookView,ShowProfileView,UpdateProfileView,UpdatePasswordView,VerifyOTPView
+from core.views import (
+    RegisterUserView,ShowUsersView,UpdateProfilePictureView, LoginUserView, AddBookView, 
+    UpdateBookView,ShowBooksView,DeleteBookView,ShowProfileView,UpdateProfileView,UpdatePasswordView,VerifyOTPView,
+    LikeBookView, GetAllGenresView,GetOwnBooksView
+            )
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('register/',RegisterUserView.as_view(),name="register"),
     path('users/',ShowUsersView.as_view(),name="show_users"),
     path('login/',LoginUserView.as_view(),name="login"),
-    path('profile_picture/<int:id>/',UploadProfilePictureView.as_view(),name="profile_picture"),
-    path('update_profile/<int:id>/',UpdateProfileView.as_view(),name="update_profile"),
+    path('profile_picture/<int:id>/',UpdateProfilePictureView.as_view(),name="profile_picture"),
+    path('update_profile/',UpdateProfileView.as_view(),name="update_profile"),
     path('update_password/',UpdatePasswordView.as_view(),name="update_password"),
     path('update_password/',UpdatePasswordView.as_view(),name="update_password"),
-    path('profile/<int:id>/',ShowProfileView.as_view(),name="show_profile"),
+    path('profile/',ShowProfileView.as_view(),name="show_profile"),
     path('add_book/',AddBookView.as_view(),name="add_book"),
     path('update_book/<int:id>/',UpdateBookView.as_view(),name="update_book"),
     path('show_books/',ShowBooksView.as_view(),name="show_books"),
     path('delete_book/<int:id>/',DeleteBookView.as_view(),name="delete_book"),
     path('verify_account/',VerifyOTPView.as_view(),name="verify_otp"),
+    path('like/',LikeBookView.as_view(), name='like_book'),
+    path('genres/',GetAllGenresView.as_view(), name="genres"),
+    path('yourbooks/',GetOwnBooksView.as_view(), name="yourbooks"),
+
+    #jwt token 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
